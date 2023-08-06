0ea6f47f00d5d7e9266bc59eca95003b9e27b733
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-08-06 02:31:11 +0100

    add dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+
+
 
 class AdminController extends Controller
 {
-    //
+    public function adminDashboard(): View
+    {
+        return view('admin.admin_dashboard');
+    }
 }
