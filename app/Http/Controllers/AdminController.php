eeec2149916d34f7c9b1498e2d7705d0590b8b3a
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-08-10 22:50:31 +0100

    add logout method

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
-
-
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function adminDashboard(): View
     {
         return view('admin.index');
+    }
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
