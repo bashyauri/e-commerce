989a1fbc559e3c55da107a76f0a82da963b81324
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-08-10 22:50:58 +0100

    add route

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ require __DIR__ . '/auth.php';
 // Admin Dashboard
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'adminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
 });
 // vendor Dashboard
 Route::middleware(['auth', 'role:vendor'])->group(function () {
