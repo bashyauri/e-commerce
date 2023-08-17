10a4b085db7a60a395eb5c3aee1b881580763ed7
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-08-17 15:49:28 +0100

    add vendor request

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Vendor\VendorRequest;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -16,6 +17,46 @@ class VendorController extends Controller
     public function login(): View
     {
         return view('vendor.login');
+    }
+    public function profile(): View
+    {
+        $user = auth()->user();
+        return view('vendor.profile', ['user' => $user]);
+    }
+    public function store(VendorRequest $request): RedirectResponse
+    {
+        $data = $request->validated();
+
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            unlink(public_path('uploads/admin_images/' . auth()->user()->photo));
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('uploads/admin_images/'), $filename);
+            $data['photo'] = $filename;
+        }
+        auth()->user()->update($data);
+
+        $notifiction = ['message' => 'Admin Profile updated!', 'alert-type' => 'success'];
+        return redirect()->back()->with($notifiction);
+    }
+    public function changePassword(): View
+    {
+        return view('admin.change-password');
+    }
+    public function updatePassword(UpdatePasswordRequest $request): RedirectResponse
+    {
+        $data = $request->validated();
+
+
+        if (!Hash::check($data['current_password'], auth()->user()->password)) {
+            return back()->with('error', 'Current password does not match');
+        }
+        auth()->user()->update([
+            'password' => Hash::make($data['new_password']),
+        ]);
+
+        return back()->with('success', 'Password has been updated');
     }
     public function logout(Request $request): RedirectResponse
     {
