0fb4a22de184a57d62aaa6d0b8024d6197852873
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-09-07 14:14:53 +0100

    remove unnecessary code

--- app/Http/Controllers/Backend/SubCategoryController.php
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -9,8 +9,7 @@ use App\Models\Category;
 use App\Models\SubCategory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+
 
 class SubCategoryController extends Controller
 {
