711fa22af9219a195c9e7ab8da2476d8cd62fc48
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-09-25 14:02:56 +0100

    added Db transaction

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -12,6 +12,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
 use Image;
 
 class ProductController extends Controller
@@ -35,42 +36,44 @@ class ProductController extends Controller
     public function storeProduct(AddProductRequest $request): RedirectResponse
     {
         $data = $request->validated();
-
-        $image = $request->file('product_thumbnail');
-        $nameGen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
-        Image::make($image)->resize(800, 800)->save('uploads/product_images/thumbnails/' . $nameGen);
-        $savedUrl = 'uploads/product_images/thumbnails/' . $nameGen;
-        $productId = Product::insertGetId([
-            'brand_id' => $data['brand_id'],
-            'product_name' => $data['product_name'],
-            'product_slug' => str()->slug($data['product_name']),
-            'product_code' => $data['product_code'],
-            'product_qty' => $data['product_qty'],
-            'product_tags' => $data['product_tags'],
-            'product_color' => $data['product_color'],
-            'short_descp' => $data['short_descp'],
-            'long_descp' => $data['long_descp'],
-            'selling_price' => $data['selling_price'],
-            'product_thumbnail' => $savedUrl,
-            'vendor_id' => $data['vendor_id'],
-            'category_id' => $data['category_id'],
-            'sub_category_id' => $data['sub_category_id'],
-            'status' => 1,
-            'created_at' => Carbon::now()
-
-        ]);
-
-        $images = $request->file('images');
-        foreach ($images as $img) {
-            $productImage = hexdec(uniqid()) . '.' . $img->getClientOriginalExtension();
-            Image::make($img)->resize(800, 800)->save('uploads/product_images/images/' . $productImage);
-            $imageUrl = 'uploads/product_images/images/' . $productImage;
-            ModelsImage::create([
-                'product_id' => $productId,
-                'photo_name' => $imageUrl,
+        DB::transaction(function () use ($data, $request) {
+            $image = $request->file('product_thumbnail');
+            $nameGen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(800, 800)->save('uploads/product_images/thumbnails/' . $nameGen);
+            $savedUrl = 'uploads/product_images/thumbnails/' . $nameGen;
+            $productId = Product::insertGetId([
+                'brand_id' => $data['brand_id'],
+                'product_name' => $data['product_name'],
+                'product_slug' => str()->slug($data['product_name']),
+                'product_code' => $data['product_code'],
+                'product_qty' => $data['product_qty'],
+                'product_tags' => $data['product_tags'],
+                'product_color' => $data['product_color'],
+                'short_descp' => $data['short_descp'],
+                'long_descp' => $data['long_descp'],
+                'selling_price' => $data['selling_price'],
+                'product_thumbnail' => $savedUrl,
+                'vendor_id' => $data['vendor_id'],
+                'category_id' => $data['category_id'],
+                'sub_category_id' => $data['sub_category_id'],
+                'status' => 1,
                 'created_at' => Carbon::now()
+
             ]);
-        }
+
+            $images = $request->file('images');
+            foreach ($images as $img) {
+                $productImage = hexdec(uniqid()) . '.' . $img->getClientOriginalExtension();
+                Image::make($img)->resize(800, 800)->save('uploads/product_images/images/' . $productImage);
+                $imageUrl = 'uploads/product_images/images/' . $productImage;
+                ModelsImage::create([
+                    'product_id' => $productId,
+                    'photo_name' => $imageUrl,
+                    'created_at' => Carbon::now()
+                ]);
+            }
+        });
+
 
         $notifiction = ['message' => 'Product Created Successfully !', 'alert-type' => 'success'];
         return redirect()->route('all.product')->with($notifiction);
