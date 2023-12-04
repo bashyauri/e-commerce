29d0058faca08efa25d395046d057c23236d2f7d
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-12-04 14:01:36 +0100

    add fashion category

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Image;
 use App\Models\Product;
 use Illuminate\Contracts\View\View;
@@ -10,6 +11,15 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+
+    public function index(): View
+    {
+        $selectedCategory = Category::skip(3)->first();
+        $selectedProduct = Product::where(['status' => 1, 'category_id' => $selectedCategory->id])
+            ->orderBy('id', 'DESC')->limit(5)->get();
+
+        return view('frontend.index', ['selectedCategory' => $selectedCategory, 'selectedProduct' => $selectedProduct]);
+    }
     public function productDetails(Product $product, string $slug): View
     {
         $productColor = explode(',', $product->product_color);
--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('frontend.index');
-});
+// Route::get('/', function () {
+//     return view('frontend.index');
+// });
+
+Route::get('/', [IndexController::class, 'index']);
 
 Route::middleware(['auth'])->group(function () {
 
